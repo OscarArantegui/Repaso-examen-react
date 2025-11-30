6501986413b98ad6bb6b237ce9aa23020176b65d
Oscar <oscar.arantegui@gmail.com>
2025-11-30 11:15:20 +0100

    Acabdo ejercicio personajes - episodios

--- personajesepisodios/src/components/CharacterCard.js
+++ personajesepisodios/src/components/CharacterCard.js
@@ -2,7 +2,7 @@ import './CharacterCard.css';
 
 function CharacterCard ({ character, onClick}) {
     return(
-        <div onclick={onClick} className="character-card">
+        <div onClick={onClick} className="character-card">
             <img src={character.image} alt={character.name}/>
             <h3 className='card-info'>{character.name}</h3>
         </div>
